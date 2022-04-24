a5753fd8861fd5ec1180b3916b3985bb1e5faaa4
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2022-04-24 18:30:20 +0700

    addd health record widget

--- lib/Screens/health_record_screen.dart
+++ lib/Screens/health_record_screen.dart
@@ -14,115 +14,151 @@ class HealthRecordScreen extends StatefulWidget {
 class _HealthRecordScreenState extends State<HealthRecordScreen> {
   final user = FirebaseAuth.instance.currentUser!;
   final database = FirebaseDatabase.instance.ref();
+  String aa = "aa";
+
+  Future<Iterable<DataSnapshot>> getdata() async {
+    DataSnapshot healthRecordRef = await database
+        .child("health-record")
+        .child(user.uid)
+        .limitToFirst(2)
+        .get();
+    Iterable<DataSnapshot> a = healthRecordRef.children;
+    // for (DataSnapshot imageSnapshot in a) {
+    //   print(imageSnapshot.key.toString());
+    // }
+    return a;
+  }
 
   bool _customTileExpanded = false;
   @override
   Widget build(BuildContext context) {
-    DatabaseReference healthRecordref =
-        database.child("health-record").child(user.uid);
-    Query healthRecord = healthRecordref.limitToFirst(5);
     return Background(
         title: "Rekam Medisku",
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 80,
-                    width: 80,
-                    child: Card(
-                      color: kLightBlue2,
-                      elevation: 4,
-                      child: InkWell(
-                        onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) => AddHealthRecordScreen())),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              Icons.edit,
-                              color: kBlack,
-                            )
-                          ],
-                        ),
+        child: Column(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 80,
+                  width: 80,
+                  child: Card(
+                    color: kLightBlue2,
+                    elevation: 4,
+                    child: InkWell(
+                      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => AddHealthRecordScreen())),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.edit,
+                            color: kBlack,
+                          )
+                        ],
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: 10,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Rekam Medis Baru',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Text(
+                        "Rekam medis dapat berupa gambar, foto, atau dokumen.",
+                        style: TextStyle(
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Rekam Medis Baru',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Text(
-                          "Rekam medis dapat berupa gambar, foto, atau dokumen.",
-                          style: TextStyle(
-                            color: Colors.black54,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 12, right: 12),
-              child: Divider(
-                color: kDarkBlue,
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 12, right: 12),
+            child: Divider(
+              color: kDarkBlue,
             ),
-            // const ExpansionTile(
-            //   title: Text(
-            //     'ExpansionTile 1',
-            //     style: TextStyle(
-            //       color: Colors.black,
-            //       fontSize: 18,
-            //       overflow: TextOverflow.ellipsis,
-            //     ),
-            //   ),
-            //   subtitle: Text(
-            //     'Trailing expansion arrow icon',
-            //     style: TextStyle(
-            //       color: Colors.black54,
-            //       overflow: TextOverflow.ellipsis,
-            //     ),
-            //   ),
-            //   children: <Widget>[
-            //     ListTile(title: Text('This is tile number 1')),
-            //   ],
-            // ),
-            Flexible(
-              child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: 15,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                        leading: Icon(Icons.list),
-                        trailing: Text(
-                          "GFG",
-                          style: TextStyle(color: Colors.green, fontSize: 15),
-                        ),
-                        title: Text("$uniqueHealthRecord $index"));
-                  }),
-            )
-          ],
-        ));
+          ),
+          FutureBuilder<Iterable<DataSnapshot>>(
+              future: getdata(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  Iterable<DataSnapshot>? healthRecordData = snapshot.data!;
+                  return Column(
+                    children: [
+                      for (DataSnapshot imageSnapshot in healthRecordData)
+                        ExpansionTile(
+                          title: Text(
+                            imageSnapshot.key!,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          subtitle: Text(
+                            'Trailing expansion arrow icon',
+                            style: TextStyle(
+                              color: Colors.black54,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          children: <Widget>[
+                            ListTile(title: Text('This is tile number 1')),
+                          ],
+                        )
+                    ],
+                  );
+                  // return Flexible(
+                  //     child: ListView.builder(
+                  //         scrollDirection: Axis.vertical,
+                  //         itemCount: 15,
+                  //         itemBuilder: (BuildContext context, int index) {
+                  //           return ExpansionTile(
+                  //             title: Text(
+                  //               "gg",
+                  //               style: TextStyle(
+                  //                 color: Colors.black,
+                  //                 fontSize: 18,
+                  //                 overflow: TextOverflow.ellipsis,
+                  //               ),
+                  //             ),
+                  //             subtitle: Text(
+                  //               'Trailing expansion arrow icon',
+                  //               style: TextStyle(
+                  //                 color: Colors.black54,
+                  //                 overflow: TextOverflow.ellipsis,
+                  //               ),
+                  //             ),
+                  //             children: <Widget>[
+                  //               ListTile(title: Text('This is tile number 1')),
+                  //             ],
+                  //           );
+                  //         }));
+                }
+              })
+        ]));
   }
 }
